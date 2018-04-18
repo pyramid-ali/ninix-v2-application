6bd1a18041baf6a725d109194674891e0a5c3050
AmirGh72 <amir_ghaffarzadeh@yahoo.com>
2018-04-18 13:01:40 +0430

    profile test has been impelemented

---
+++ __tests__/profile.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Profile from '../App/Containers/Profile';
+
+const initialState = {
+    baby: {name: null, birthDate: null, gender: null, image: null, progress: null, bloodGroup: null, changeDate: null, sync: true},
+    father: {name: null, birthDate: null, job: null, mobile: null, bloodGroup: null, phone: null, email: null, image: null, progress: null, sync: true, updatedAt: null},
+    mother: {name: null, birthDate: null, job: null, mobile: null, bloodGroup: null, phone: null, email: null, image: null, progress: null, sync: true, updatedAt: null}
+};
+let wrapper, profileContainer, navigation, store;
+
+describe('Test Profile', () => {
+
+  beforeEach(() => {
+    console.error = jest.fn() //hide console.error warnings
+    store = mockStore(initialState)
+    navigation = { addListener: jest.fn() } //navigation property
+    wrapper = shallow(<Profile navigation={navigation}/>, { context: { store } }) 
+    profileContainer = wrapper.dive() // get one level deep in wrapper
+  });
+
+  test('check snapshot', () => {
+    expect(profileContainer).toMatchSnapshot()
+  });
+
+  test('container rendered correctly', () => {
+    expect(profileContainer.length).toEqual(1)
+  });
+
+  test('container childrens called correctly', () => {
+    expect(profileContainer.find('EditableImage').length).toEqual(1)
+  });
+
+  test('container props matches with initialState', () => {
+    expect(wrapper.prop('baby')).toEqual(initialState.baby)
+    expect(wrapper.prop('father')).toEqual(initialState.father)
+    expect(wrapper.prop('mother')).toEqual(initialState.mother)
+  });
+
+});
