46d9ea7cda236f686dd86f5267fdd61322325ce7
AmirGh72 <amir_ghaffarzadeh@yahoo.com>
2018-04-22 16:09:39 +0430

    essentialInformationSlider test has been impelemented

---
+++ __tests__/slider.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import EssentialInformationSlider from '../App/Containers/EssentialInformationSlider';
+
+let wrapper, sliderContainer, store;
+
+describe('Test slider', () => {
+
+  beforeEach(() => {
+    console.error = jest.fn() //hide console.error warnings
+    store = mockStore()
+    wrapper = shallow(<EssentialInformationSlider />, { context: { store } }) 
+    sliderContainer = wrapper.dive() //get one level deep in wrapper
+  });
+
+  test('check snapshot', () => {
+    expect(sliderContainer).toMatchSnapshot()
+  });
+
+  test('container rendered correctly', () => {
+    expect(sliderContainer.length).toEqual(1)
+  });
+
+  test('container childrens called correctly', () => {
+    expect(sliderContainer.find('SingleInputPage').length).toEqual(1)
+  });
+
+  test('container state updated correctly', () => {
+    sliderContainer.find('SingleInputPage').at(0).simulate('change', 100)
+    expect(sliderContainer.state('height')).toEqual(100)
+  });
+
+});
