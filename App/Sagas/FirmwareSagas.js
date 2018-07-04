fdc4e2bdb4fa7ac600da2362ea0bf37eb1340371
Ali Shabani <pyramid.note@yahoo.com>
2018-07-04 11:13:23 +0430

    replace device with ninix and correct getting latest firmware if necessary

--- App/Sagas/FirmwareSagas.js
+++ App/Sagas/FirmwareSagas.js
@@ -12,8 +12,8 @@ import Api from '../Services/Api'
 
 export function *checkLatestVersion(api) {
 
-  const { auth, device } = yield select()
-  if (!device.firmware) {
+  const { auth, ninix } = yield select()
+  if (!ninix.firmware) {
     yield put(FirmwareAction.didFail(null))
     return
   }
@@ -22,12 +22,10 @@ export function *checkLatestVersion(api) {
   try {
     const result = yield call(Response.resolve, response)
     let path = null
-    // if (result.firmware.version > device.firmware) {
-    //   const firmwareFile = yield call(Api.download, result.firmware.path, auth.accessToken)
-    //   path = firmwareFile.path()
-    // }
-    const firmwareFile = yield call(Api.download, result.firmware.path, auth.accessToken)
-    path = firmwareFile.path()
+    if (result.firmware.version > ninix.firmware) {
+      const firmwareFile = yield call(Api.download, result.firmware.path, auth.accessToken)
+      path = firmwareFile.path()
+    }
     yield put(FirmwareAction.setLatestVersion({
       ...JsonToModel.firmware(result.firmware),
       path
@@ -35,27 +33,25 @@ export function *checkLatestVersion(api) {
   }
   catch (e) {
     yield put(FirmwareAction.didFail(e.message || 'something went wrong while checking latest firmware version, try again'))
-    console.tron.log({e})
   }
 
 }
 
 export function *startUpdate() {
-  const res = yield call(CentralManager.startUpdate.bind(CentralManager))
-  console.tron.log({res})
+  yield call([CentralManager, CentralManager.startUpdate])
 }
 
 export function *updateFirmware() {
-  const { firmware, device } = yield select()
+  const { firmware, ninix } = yield select()
   yield fork(setupDFUProgress, yield call(setupDFUProgressChannel))
   yield fork(setupDFUStateChange, yield call(setupDFUStateChangeChannel))
   try {
-    const res = yield call(CentralManager.updateFirmware.bind(CentralManager), firmware.path)
+    yield call([CentralManager, CentralManager.updateFirmware], firmware.path)
     yield put(FirmwareAction.didUpdateSuccess())
-    yield put(BluetoothAction.connect(device.device))
+    yield put(BluetoothAction.connect(ninix.device))
   }
   catch (e) {
-    yield put(FirmwareAction.didUpdateSuccess(e.message))
+    yield put(FirmwareAction.didUpdateFail(e.message))
   }
 
 }
